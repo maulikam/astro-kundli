c39b199e830777e4b59e063b14fbb4ec3d82b8c0
maulikam <mdave.5191@gmail.com>
2024-09-30 16:08:09 +0530

    WIP AstrologyCalculations

--- src/main/java/co/renil/astro/kundli/service/AstrologyInterpretationService.java
+++ src/main/java/co/renil/astro/kundli/service/AstrologyInterpretationService.java
@@ -6,13 +6,16 @@ import org.springframework.stereotype.Service;
 public class AstrologyInterpretationService {
     public String interpretNatalChart(/* parameters */) {
         // Implementation
+        return null;
     }
 
     public String interpretTransits(/* parameters */) {
         // Implementation
+        return null;
     }
 
     public String interpretSynastry(/* parameters */) {
         // Implementation
+        return null;
     }
 }
